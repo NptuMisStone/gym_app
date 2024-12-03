0f63cab37f56a7dac75ee46525a89b428e6ea347
Youchenjiang <1014308g@gmail.com>
2024-12-04 00:11:38 +0800

    v1.111：上線啦 發佈應用程式到Play商店封閉測試(侑宸)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android { //rename package:https://stackoverflow.com/questions/16804093/rename-p
         applicationId = "com.NPTUMisStone.gym_app"
         minSdk = 28
         targetSdk = 34
-        versionCode = 111
-        versionName = "0.111"
+        versionCode = 1111
+        versionName = "1.111"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
