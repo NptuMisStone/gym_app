a2ed1f6c6bf58d840560e759ea53c4b97e48f19c
NptuMisStone <nptumisstone@gmail.com>
2024-12-05 22:01:41 +0800

    v1.123：更新常見問題介面的固定問題(侑宸)

--- app/src/main/java/com/NPTUMisStone/gym_app/User_And_Coach/UI/AIInteractive.java
+++ app/src/main/java/com/NPTUMisStone/gym_app/User_And_Coach/UI/AIInteractive.java
@@ -1,8 +1,15 @@
 package com.NPTUMisStone.gym_app.User_And_Coach.UI;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
 import android.widget.EditText;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -18,6 +25,8 @@ import com.NPTUMisStone.gym_app.R;
 import com.NPTUMisStone.gym_app.User_And_Coach.Helper.ProgressBarHandler;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import okhttp3.MediaType;
@@ -50,6 +59,96 @@ public class AIInteractive extends AppCompatActivity {
 
     private Retrofit retrofitInstance;
 
+    static class Question {
+        String title;
+        String answer;
+
+        public Question(String title, String answer) {
+            this.title = title;
+            this.answer = answer;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getAnswer() {
+            return answer;
+        }
+    }
+
+    static class ExpandableQuestionAdapter extends BaseExpandableListAdapter {
+        Context context;
+        List<Question> questions;
+
+        public ExpandableQuestionAdapter(Context context, List<Question> questions) {
+            this.context = context;
+            this.questions = questions;
+        }
+
+        @Override
+        public int getGroupCount() {
+            return questions.size();
+        }
+
+        @Override
+        public int getChildrenCount(int groupPosition) {
+            return 1; // 每個問題只有一個解答
+        }
+
+        @Override
+        public Object getGroup(int groupPosition) {
+            return questions.get(groupPosition);
+        }
+
+        @Override
+        public Object getChild(int groupPosition, int childPosition) {
+            return questions.get(groupPosition).getAnswer();
+        }
+
+        @Override
+        public long getGroupId(int groupPosition) {
+            return groupPosition;
+        }
+
+        @Override
+        public long getChildId(int groupPosition, int childPosition) {
+            return childPosition;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+            String title = ((Question) getGroup(groupPosition)).getTitle();
+            if (convertView == null) {
+                convertView = LayoutInflater.from(context).inflate(android.R.layout.simple_expandable_list_item_1, parent, false);
+            }
+            TextView textView = convertView.findViewById(android.R.id.text1);
+            textView.setText(title);
+            return convertView;
+        }
+
+        @Override
+        public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+            String answer = (String) getChild(groupPosition, childPosition);
+            if (convertView == null) {
+                convertView = LayoutInflater.from(context).inflate(android.R.layout.simple_expandable_list_item_2, parent, false);
+            }
+            TextView textView = convertView.findViewById(android.R.id.text1);
+            textView.setText(answer);
+            return convertView;
+        }
+
+        @Override
+        public boolean isChildSelectable(int groupPosition, int childPosition) {
+            return true;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,7 +160,7 @@ public class AIInteractive extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        progressBarHandler = new ProgressBarHandler(this,findViewById(android.R.id.content));
+        progressBarHandler = new ProgressBarHandler(this, findViewById(android.R.id.content));
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -69,9 +168,22 @@ public class AIInteractive extends AppCompatActivity {
             return insets;
         });
         checkHealth();
+
         findViewById(R.id.AIInteractive_submitButton).setOnClickListener(v -> sendRequest(((EditText) findViewById(R.id.AIInteractive_inputEdit)).getText().toString()));
         findViewById(R.id.AIInteractive_backButton).setOnClickListener(v -> finish());
+
+        List<Question> questions = new ArrayList<>();
+        questions.add(new Question("如何知道健身教練的詳細資訊及安排的課程？", "在健身教練頁面，點擊任何一個教練即可查看教練詳細資訊及他所有安排的課程。"));
+        questions.add(new Question("想知道如何預約課程？", "在尋找課程中會有各個教練安排的課程供你們選擇，課程點擊後，即可依照課程日期、時間進行預約。"));
+        questions.add(new Question("想知道如何預約課程？", "在尋找課程中會有各個教練安排的課程供你們選擇，課程點擊後，即可依照課程日期、時間進行預約。"));
+        questions.add(new Question("如何查找特定課程？", "在尋找課程中，搜尋列表旁，有個篩選按鈕，可進行選取想查詢的課程。"));
+        questions.add(new Question("如何查看目前所收藏的教練及課程？", "在我的收藏中，即可看到所收藏的教練及課程。"));
+
+        ExpandableListView expandableListView = findViewById(R.id.AIInteractive_expandableList);
+        ExpandableQuestionAdapter adapter = new ExpandableQuestionAdapter(this, questions);
+        expandableListView.setAdapter(adapter);
     }
+
 
     private void sendRequest(String inputText) {
         progressBarHandler.showProgressBar();
